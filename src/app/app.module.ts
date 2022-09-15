0cdcfef6bbf088831e4cc0516e70c14ca8aac141
Carlos Gottret Murillo <mauriciogottret@MacBook-Pro-3.local>
2022-09-15 15:38:53 -0400

    Change

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    AboutMeComponent,
+    ProjectsComponent,
+    ContactMeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
